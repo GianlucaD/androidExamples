88ffcdcac2a21e7e3c3853129ff81e27bb8aedaf
GianlucaD <Up1nsm0ke!>
2020-04-12 12:57:53 +0200

    init LifeCycleDemo

---
+++ LIfeCycleDemo/app/src/main/java/ch/noseryoung/lifecycledemo/Main2Activity.java
@@ -0,0 +1,42 @@
+package ch.noseryoung.lifecycledemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+}
---
+++ LIfeCycleDemo/app/src/main/java/ch/noseryoung/lifecycledemo/MainActivity.java
@@ -0,0 +1,23 @@
+package ch.noseryoung.lifecycledemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openActivity2(View v){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
