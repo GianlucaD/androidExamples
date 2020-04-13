b9fa0c0cf35b0bd1fcb73eaf847b6f2dc4f44340
GianlucaD <luke@daffre.com>
2020-04-13 08:54:03 +0200

    Update MainActivity.java
    small changes

--- LIfeCycleDemo/app/src/main/java/ch/noseryoung/lifecycledemo/MainActivity.java
+++ LIfeCycleDemo/app/src/main/java/ch/noseryoung/lifecycledemo/MainActivity.java
@@ -16,8 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //starts a new activity
     public void openActivity2(View v){
         Intent intent = new Intent(this, Main2Activity.class);
+        // if you want to get back, clear stack
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
