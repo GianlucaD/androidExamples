c1cd7d9a23136ef9d59a666f3ee40cc5f933b21f
Gianluca Daffré <gianluca.daffre@noseryoung.ch>
2020-04-18 12:33:51 +0200

    added ListviewDemo

---
+++ ListviewDemo/app/src/main/java/ch/noseryoung/listviewdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package ch.noseryoung.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ArrayList mDataset = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDataset.add("Gianluca1");
+        mDataset.add("Gianluca2");
+        mDataset.add("Gianluca3");
+        mDataset.add("Gianluca4");
+        mDataset.add("Gianluca5");
+        mDataset.add("Gianluca6");
+        mDataset.add("Gianluca7");
+        mDataset.add("Gianluca8");
+        mDataset.add("Gianluca9");
+        mDataset.add("Gianluca10");
+        mDataset.add("Gianluca11");
+        mDataset.add("Gianluca12");
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.itemsView);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        MyAdapter mAdapter = new MyAdapter(mDataset);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+
+
+}
